92f84e621314bca0041f16241a31507aa927b983
LuisTakeo <luistakeo@hotmail.com>
2024-03-20 21:53:45 -0300

    App contador feito

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,80 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Contador(),));
+  runApp(MaterialApp(
+    home: Contador(),
+  ));
 }
 
-
 class Contador extends StatefulWidget {
-  const Contador({ Key? key }) : super(key: key);
+  const Contador({Key? key}) : super(key: key);
 
   @override
   _ContadorState createState() => _ContadorState();
 }
 
-void  decrement()
-{
-  print("Decrement");
-}
-
-void  increment()
-{
-  print("Increment");
-}
-
 class _ContadorState extends State<Contador> {
+  int count = 0;
+
+  void decrement() {
+    setState(() {
+      if (count > 0) count--;
+    });
+    print(count);
+  }
+
+  void increment() {
+    setState(() {
+      count++;
+    });
+    print(count);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-    (
+    return Scaffold(
       backgroundColor: Colors.red,
       // appBar: AppBar(title: Text("Contador de pessoas.")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:
-        [
-          Text
-          (
-            "You can enter.\n", 
-            style: TextStyle
-            (
-              fontSize: 30,
-              color: Colors.white
-            ),
-          ),
-          Text
-          (
-            "0",
-            style: TextStyle
-            (
-              fontSize: 100,
-              color: Colors.white
-            ),
-          ),
-          Row
-          (
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: 
-            [
-              TextButton
-              (
-                onPressed: decrement, 
-                style: TextButton.styleFrom
-                (
-                  backgroundColor: Colors.white,
-                  fixedSize: Size(100, 100),
-                  shape: RoundedRectangleBorder
-                  (
-                    borderRadius: BorderRadius.circular(24)
-                  )
-                ),
-                child: Text
-                (
-                  "Saiu",
-                  style: TextStyle
-                  (
-                    fontSize: 16,
-                    color: Colors.black
-                  ),
-                )
-              ),
-              SizedBox(width: 64,),
-              TextButton
-              (
-                onPressed: increment, 
-                style: TextButton.styleFrom
-                (
-                  backgroundColor: Colors.white,
-                  fixedSize: Size(100, 100),
-                  shape: RoundedRectangleBorder
-                  (
-                    borderRadius: BorderRadius.circular(24)
-                  )
-                ),
-                child: Text
-                (
-                  "Entrou",
-                  style: TextStyle
-                  (
-                    fontSize: 16,
-                    color: Colors.black
-                  ),
-                )
-                )
-            ],
-          )
-        ]
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(
+          "You can enter.\n",
+          style: TextStyle(fontSize: 30, color: Colors.white),
         ),
+        Text(
+          count.toString(),
+          style: TextStyle(fontSize: 100, color: Colors.white),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: decrement,
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    fixedSize: Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24))),
+                child: Text(
+                  "Saiu",
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                )),
+            SizedBox(
+              width: 64,
+            ),
+            TextButton(
+                onPressed: increment,
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    fixedSize: Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24))),
+                child: Text(
+                  "Entrou",
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ))
+          ],
+        )
+      ]),
     );
-
   }
 }
