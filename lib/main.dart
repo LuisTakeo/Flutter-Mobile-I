2fbebeea7f8aa346096630877ab24bf5ad419cbf
LuisTakeo <luistakeo@hotmail.com>
2024-03-25 21:46:25 -0300

    Finalização do app de contador

--- lib/main.dart
+++ lib/main.dart
@@ -20,61 +20,63 @@ class _ContadorState extends State<Contador> {
     setState(() {
       if (count > 0) count--;
     });
-    print(count);
   }
 
   void increment() {
     setState(() {
-      count++;
+      if (count < 30) count++;
     });
-    print(count);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      // appBar: AppBar(title: Text("Contador de pessoas.")),
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
-          "You can enter.\n",
-          style: TextStyle(fontSize: 30, color: Colors.white),
-        ),
-        Text(
-          count.toString(),
-          style: TextStyle(fontSize: 100, color: Colors.white),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    fixedSize: Size(100, 100),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24))),
-                child: Text(
-                  "Saiu",
-                  style: TextStyle(fontSize: 16, color: Colors.black),
-                )),
-            SizedBox(
-              width: 64,
+        backgroundColor: Colors.red,
+        // appBar: AppBar(title: Text("Contador de pessoas.")),
+        body: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("img/restaurant.png"), fit: BoxFit.cover)),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              "You can enter.\n",
+              style: TextStyle(fontSize: 30, color: Colors.white),
             ),
-            TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    fixedSize: Size(100, 100),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24))),
-                child: Text(
-                  "Entrou",
-                  style: TextStyle(fontSize: 16, color: Colors.black),
-                ))
-          ],
-        )
-      ]),
-    );
+            Text(
+              count.toString(),
+              style: TextStyle(fontSize: 100, color: Colors.white),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: decrement,
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        fixedSize: Size(100, 100),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24))),
+                    child: Text(
+                      "Saiu",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    )),
+                SizedBox(
+                  width: 64,
+                ),
+                TextButton(
+                    onPressed: increment,
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        fixedSize: Size(100, 100),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24))),
+                    child: Text(
+                      "Entrou",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ))
+              ],
+            )
+          ]),
+        ));
   }
 }
